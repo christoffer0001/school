d196e7cb32a87833521d562eaf427f316ebb603e
christoffer0001 <chbe0001@edu.mercantec.dk>
2025-01-30 13:17:10 +0100

    Update sketch.js

--- 10 Print/sketch.js
+++ 10 Print/sketch.js
@@ -1,45 +1,31 @@
-const x = 1500;
-const y = 771;
-let lineLength = 15;    ll = lineLength;
-lineThickness = 2.5;  lt = lineThickness;
+let size = 40;
+let rounds = 4;
+let width = 800, height = 800;
 
-let buffer = 5;
-let numberOfFigurs = 2; nof = numberOfFigurs;
-
-function setup() {   
-    createCanvas(x, y);
-    //thickness
-    strokeWeight(lt);
-
-    for(let b=0;b < x; b = b+1){
-        for(let h=0;h < y; h = h+1){
-            
-            let x1 = ll*b;
-            let y1 = ll*h;
-
-            let x2 = ll*b;
-            let y2 = ll*h;
+function setup() {
+    createCanvas(width + 10, height + 20);
+    background(253);
+    noFill();
 
 
-            let r = Math.floor((Math.random() * nof)+1);
-
-            if(r==1) { //left to right
-                line(x1-ll + buffer,y1-ll + buffer, x1 + buffer, y1 + buffer);
-    
-            }else if(r==2){ //right to left
-                line(x2 + buffer,y2-ll + buffer, x2-ll + buffer, y2 + buffer);
-                
-            }else if (r==3){ //x figur
-                line(x1-ll + buffer,y1-ll + buffer, x1 + buffer, y1 + buffer);
-                line(x2 + buffer,y2-ll + buffer, x2-ll + buffer, y2 + buffer);
-
-            }else if (r=4) { //cross
-                
-                line(x2+buffer, y2, x2-ll+buffer, y2);
-                line(x2+buffer-(ll/2), y2-(ll/2), x2+buffer-(ll/2), y2+(ll/2));
-                
+    for (let k = 0; k < rounds; k++) {
+     
+        for (let j = 1; j < width / size; j++) {
+            for (let i = 1; i < width / size; i++) {
+                strokeWeight(random(3));
+                stroke(random(255), random(255), random(255));
+                //quad(0 + size*i + random(5), 0 + size * i + random(5), size + size * i + random(5), 0 + size * i + random(5), size + size * i + random(5), 0 + size * i + random(5), 0 + size*i + random(5), 0 + size * i + random(5));
+                quad(
+                    0 + size * j + random(5), 0 + size * i + random(5),
+                    size + size * j + random(5), 0 + size * i + random(5),
+                    size + size * j + random(5), size + size * i + random(5),
+                    0 + size * j + random(5), size + size * i + random(5)
+                );
             }
         }
-    }   
+    }
 }
 
+const random = function(q) {
+    return Math.floor(Math.random() * q) + 1;
+}
